b2e47341a0ca1e7a4e517b84857fd3ce249cd76b
nijesh <niju.victor@gmail.com>
2021-11-12 10:48:57 +0800

    OpenScale Format Changes

--- app.py
+++ app.py
@@ -20,16 +20,13 @@ def prediction():
     print("Printing Request Data")
     print(request_data)
     
-    values = request_data['input_data'][0]['values']
-    fields = request_data['input_data'][0]['fields']
+    values = request_data['values']
+    fields = request_data['fields']
     
     data = pd.DataFrame(values,columns=fields)
    
-    
     with open('pipeline-model.pkl', 'rb') as inp:
         import os
-        print(os.getcwd())
-        print(os.listdir())
         mod = pickle.load(inp)
     
     fields = ['Count', 'Country', 'State', 'City', 'Latitude', 'Longitude', 'Partner', 'Dependents', 
@@ -40,33 +37,36 @@ def prediction():
     test_data = data
     X_test = test_data[fields]
     
-    #y_test = test_data['ChurnValue']
-    
-    prediction=mod.predict(X_test)
-    #print(prediction)
-    
     y_pred =  mod.predict(X_test).tolist()
     y_prob =  mod.predict_proba(X_test).tolist()
 
-    vals = list(zip(y_pred,y_prob))
-    
+    new_fields = fields.copy()
+
+    fields.append('prediction')
+    fields.append('probability')
+
+    values = test_data.values.tolist()
+            
+    i = 0
+    while i < len(y_pred) :
+        values[i].append(y_pred[i])
+        i +=1
+
+    j=0  
+    while j < len(y_prob) :
+        values[j].append(y_prob[j])
+        j +=1
+        
+        
     format = {
-        "predictions": [
-            {
-                "fields": [
-                    "prediction",
-                    "probability"
-                ],
-                "values": vals
-            }
-        ]
+        
+        "fields":fields,
+        "labels":[1,0],
+        "values":values  
     }
     
     
     return format
-
-
-
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
